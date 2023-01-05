365f1880bdf1adb399532c8d8ee476e2fd91d5de
Yennefer of Vengerberg <5850742+yenneferofvengerberg@users.noreply.github.com>
2023-01-05 22:11:49 +0100

    Add task utilities

---
+++ Tasks/TaskExtensions.cs
@@ -0,0 +1,39 @@
+﻿// ==++==
+//
+//   Copyright (c) Shadowsoft Corporation.  All rights reserved.
+//
+// ==--==
+
+namespace Yextly.Tasks
+{
+    /// <summary>
+    /// Contains general purpose asynchronous extensions.
+    /// </summary>
+    public static class AsyncExtensions
+    {
+        /// <summary>
+        /// Turns an asynchronous <see cref="Task{T}"/> into a synchronous one.
+        /// </summary>
+        /// <typeparam name="T">The type of the task.</typeparam>
+        /// <param name="task">The task.</param>
+        /// <returns></returns>
+        public static T AsSync<T>(this Task<T> task)
+        {
+            ArgumentNullException.ThrowIfNull(task);
+
+            return task.ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// Turns an asynchronous <see cref="Task"/> into a synchronous one.
+        /// </summary>
+        /// <param name="task">The task.</param>
+        /// <returns></returns>
+        public static void AsSync(this Task task)
+        {
+            ArgumentNullException.ThrowIfNull(task);
+
+            task.ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+    }
+}
