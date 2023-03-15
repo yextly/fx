84a983e39058185a5d514d4794e457de873edbe1
Yennefer of Vengerberg <5850742+yenneferofvengerberg@users.noreply.github.com>
2023-03-15 13:58:48 +0100

    Add tests

--- CommonUnitTests/HybridStreamTests.cs
+++ CommonUnitTests/HybridStreamTests.cs
@@ -7,6 +7,7 @@
 using System;
 using System.IO;
 using Xunit;
+using Xunit.Abstractions;
 using Yextly.Common;
 
 namespace CommonUnitTests
@@ -14,6 +15,38 @@ namespace CommonUnitTests
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "CA5394:Do not use insecure randomness", Justification = "We don't deal with security")]
     public sealed class HybridStreamTests
     {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public HybridStreamTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [InlineData(128, 1, 1, 128, 1)]
+        [InlineData(128, 1, 1, 128, 2)]
+        [InlineData(128, 1, 1, 129, 1)]
+        [InlineData(128, 1, 1, 130, 1)]
+        [InlineData(128, 1, 1, 131, 1)]
+        [InlineData(128, 1, 1, 132, 1)]
+        [InlineData(128, 1, 1, 132, 2)]
+        [InlineData(128, 1, 1, 132, 3)]
+        [InlineData(128, 1, 1, 132, 4)]
+        [InlineData(128, 1, 1, 132, 5)]
+        [InlineData(128, 2, 3, 132, 2)]
+        [InlineData(128, 2, 3, 133, 2)]
+        [InlineData(128, 2, 3, 133, 3)]
+        [InlineData(128, 3, 4, 133, 5)]
+        [InlineData(117, 3, 2, 151, 4)]
+        [InlineData(117, 5, 4, 151, 4)]
+        [InlineData(117, 5, 4, 151, 5)]
+        [InlineData(117, 5, 5, 151, 5)]
+        [InlineData(116, 5, 5, 151, 5)]
+        [InlineData(116, 5, 5, 152, 5)]
+        [InlineData(116, 5, 5, 152, 6)]
+        [InlineData(116, 6, 5, 152, 6)]
+        [InlineData(116, 6, 2, 152, 6)]
+        [InlineData(116, 6, 3, 152, 6)]
+        [InlineData(116, 6, 4, 152, 6)]
         [InlineData(128, 3, 4, 129, 5)]
         [InlineData(128, 3, 4, 131, 5)]
         [InlineData(128, 3, 3, 131, 5)]
@@ -51,6 +84,8 @@ namespace CommonUnitTests
             var array = new byte[count];
             random.NextBytes(array);
 
+            _testOutputHelper.WriteLine("Writing...");
+
             int index = 0;
             while (count > 0)
             {
@@ -61,11 +96,31 @@ namespace CommonUnitTests
             }
 
             var buffer = new byte[bufferSize];
-            while (true)
+
+            _testOutputHelper.WriteLine("Reading forward...");
+            for (int i = 0; i < maxLength; i++)
             {
-                var r = dual.Read(buffer, 0, buffer.Length);
-                if (r == 0)
-                    break;
+                dual.Seek(0, SeekOrigin.Begin);
+
+                while (true)
+                {
+                    var r = dual.Read(buffer, 0, buffer.Length);
+                    if (r == 0)
+                        break;
+                }
+            }
+
+            _testOutputHelper.WriteLine("Reading backward...");
+            for (int i = length - 1; i >= 0; i--)
+            {
+                dual.Seek(0, SeekOrigin.End);
+
+                while (true)
+                {
+                    var r = dual.Read(buffer, 0, buffer.Length);
+                    if (r == 0)
+                        break;
+                }
             }
         }
 
@@ -116,7 +171,9 @@ namespace CommonUnitTests
                 return stream;
             }
             else
+            {
                 return new MemoryStream(buffer, false);
+            }
         }
     }
 }
