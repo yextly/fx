587fdccaa6e7519ae235ebf7eb3ee31179954c76
Yennefer of Vengerberg <5850742+yenneferofvengerberg@users.noreply.github.com>
2023-03-16 07:51:14 +0100

    Allow the tests to see the internals

---
+++ Common/Global.cs
@@ -0,0 +1,9 @@
+﻿// ==++==
+//
+//   Copyright (c) Shadowsoft Corporation.  All rights reserved.
+//
+// ==--==
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("CommonUnitTests")]
