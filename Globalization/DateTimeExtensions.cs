696a04cf98666f092582b8baceff01b76df6ee3d
Yennefer of Vengerberg <5850742+yenneferofvengerberg@users.noreply.github.com>
2023-09-21 19:43:07 +0200

    Add Iso 8601 formatting extension

---
+++ Globalization/DateTimeExtensions.cs
@@ -0,0 +1,24 @@
+﻿// ==++==
+//
+//   Copyright (c) Shadowsoft Corporation.  All rights reserved.
+//
+// ==--==
+
+namespace System
+{
+    /// <summary>
+    /// Provides extension to <see cref="DateTime"/>.
+    /// </summary>
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Converts the value of the specified <paramref name="value"/> object to its equivalent string representation using the ISO format.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <returns></returns>
+        public static string ToIso8601String(this DateTime value)
+        {
+            return value.ToString("O", System.Globalization.CultureInfo.InvariantCulture);
+        }
+    }
+}
