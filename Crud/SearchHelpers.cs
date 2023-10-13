199c7e3df261492a168e5d1eba7c70cdf502d4f8
Yennefer of Vengerberg <5850742+yenneferofvengerberg@users.noreply.github.com>
2023-10-13 08:42:27 +0200

    Add in memory like operation with null protection (#13)

---
+++ Crud/SearchHelpers.cs
@@ -0,0 +1,22 @@
+﻿// ==++==
+//
+//   Copyright (c) Shadowsoft Corporation.  All rights reserved.
+//
+// ==--==
+
+namespace Yextly.ServiceFabric.Mvc.Crud
+{
+    internal sealed class SearchHelpers
+    {
+        public static bool Like(string matchExpression, string pattern)
+        {
+            if (string.IsNullOrEmpty(matchExpression))
+                return false;
+
+            if (string.IsNullOrEmpty(pattern))
+                return true;
+
+            return matchExpression.Contains(pattern, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
