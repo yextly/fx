a743e0ec8cfc8846efb9452cef996cbb5003768f
Yennefer of Vengerberg <5850742+yenneferofvengerberg@users.noreply.github.com>
2023-11-10 14:53:13 +0100

    Fix improper assembly SemVer (#24)
    (cherry picked from commit fb433407c494b14464dd0dc61d620481ef63ec00)

--- Shared/AssemblyInfo.cs
+++ Shared/AssemblyInfo.cs
@@ -6,6 +6,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyInformationalVersion(ThisAssembly.Git.SemVer.Major + "." + ThisAssembly.Git.SemVer.Minor + "." + ThisAssembly.Git.Commits + "-" + ThisAssembly.Git.Branch + "+" + ThisAssembly.Git.Commit)]
+[assembly: AssemblyInformationalVersion(ThisAssembly.Git.SemVer.Major + "." + ThisAssembly.Git.SemVer.Minor + "." + ThisAssembly.Git.SemVer.Patch + "+" + ThisAssembly.Git.Sha)]
 [assembly: AssemblyFileVersion(ThisAssembly.Git.SemVer.Major + "." + ThisAssembly.Git.SemVer.Minor + "." + ThisAssembly.Git.SemVer.Patch + "." + ThisAssembly.Git.Commits)]
 [assembly: AssemblyVersion(ThisAssembly.Git.BaseVersion.Major + "." + ThisAssembly.Git.BaseVersion.Minor + "." + ThisAssembly.Git.BaseVersion.Patch)]
