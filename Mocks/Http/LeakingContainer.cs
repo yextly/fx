6b5793b7ab363a68671249b4f32395efa89ba33e
Yennefer of Vengerberg <5850742+yenneferofvengerberg@users.noreply.github.com>
2024-01-12 20:04:01 +0100

    Add empty implementations

---
+++ Mocks/Http/LeakingContainer.cs
@@ -0,0 +1,59 @@
+﻿// ==++==
+//
+//   Copyright (c) Shadowsoft Corporation.  All rights reserved.
+//
+// ==--==
+
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using Yextly.Common;
+
+namespace Yextly.Testing.Mocks.Http
+{
+    internal sealed class LeakingContainer : IDisposableProducerConsumerCollection<IDisposable>
+    {
+        public int Count => 0;
+
+        public bool IsSynchronized => false;
+
+        public object SyncRoot { get; } = new();
+
+        public void CopyTo(IDisposable[] array, int index)
+        {
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public IEnumerator<IDisposable> GetEnumerator()
+        {
+            return Enumerable.Empty<IDisposable>().GetEnumerator(); 
+        }
+
+        public IDisposable[] ToArray()
+        {
+            return Array.Empty<IDisposable>();
+        }
+
+        public bool TryAdd(IDisposable item)
+        {
+            return true;
+        }
+
+        public bool TryTake([MaybeNullWhen(false)] out IDisposable item)
+        {
+            item = default;
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Enumerable.Empty<IDisposable>().GetEnumerator();
+        }
+    }
+}
---
+++ Mocks/Http/NullLogger.cs
@@ -0,0 +1,32 @@
+﻿// ==++==
+//
+//   Copyright (c) Shadowsoft Corporation.  All rights reserved.
+//
+// ==--==
+
+using Microsoft.Extensions.Logging;
+
+namespace Yextly.Testing.Mocks.Http
+{
+    internal sealed class NullLogger : ILogger, IDisposable
+    {
+        public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+        {
+            return this;
+        }
+
+        public void Dispose()
+        {
+            // nothing here on purpose
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return false;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+        }
+    }
+}
