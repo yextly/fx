00d55458c384079039eaeb493908cbff8bd35d14
Yennefer of Vengerberg <5850742+yenneferofvengerberg@users.noreply.github.com>
2024-01-12 19:56:16 +0100

    Add disposable collections

---
+++ CommonUnitTests/DisposableObject.cs
@@ -0,0 +1,23 @@
+﻿// ==++==
+//
+//   Copyright (c) Shadowsoft Corporation.  All rights reserved.
+//
+// ==--==
+
+using System;
+
+namespace CommonUnitTests
+{
+    internal sealed class DisposableObject : IDisposable
+    {
+        public bool Disposed { get; private set; }
+
+        public void Dispose()
+        {
+            if (Disposed)
+                throw new InvalidOperationException("Double dispose.");
+
+            Disposed = true;
+        }
+    }
+}
