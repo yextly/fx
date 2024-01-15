3759655c20480e76c689395d05bdc9f78e301bf5
Yennefer of Vengerberg <5850742+yenneferofvengerberg@users.noreply.github.com>
2024-01-15 11:13:27 +0100

    Add chain operation flow

---
+++ Mocks/Http/OperationFlow.cs
@@ -0,0 +1,31 @@
+﻿// ==++==
+//
+//   Copyright (c) Shadowsoft Corporation.  All rights reserved.
+//
+// ==--==
+
+using System.Net;
+
+namespace Yextly.Testing.Mocks.Http
+{
+    internal sealed class OperationFlow
+    {
+        public OperationFlow(HttpMethod expectedMethod, Uri expectedUri, HttpStatusCode statusCode, HttpContent? content, Action<HttpRequestMessage, HttpResponseMessage>? action)
+        {
+            ArgumentNullException.ThrowIfNull(expectedMethod);
+            ArgumentNullException.ThrowIfNull(expectedUri);
+
+            ExpectedMethod = expectedMethod;
+            ExpectedUri = expectedUri;
+            StatusCode = statusCode;
+            Content = content;
+            Action = action;
+        }
+
+        public Action<HttpRequestMessage, HttpResponseMessage>? Action { get; }
+        public HttpContent? Content { get; }
+        public HttpMethod ExpectedMethod { get; }
+        public Uri? ExpectedUri { get; }
+        public HttpStatusCode StatusCode { get; }
+    }
+}
