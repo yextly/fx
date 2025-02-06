5525bd0f51c16c0680b18de85210ccb762526dd7
Yennefer of Vengerberg <5850742+yenneferofvengerberg@users.noreply.github.com>
2025-02-06 21:45:31 +0100

    Add the scripting utilities for single file application

---
+++ ScriptingServicesSFA/ScriptingExecutionContextBuilderExtensions.cs
@@ -0,0 +1,65 @@
+﻿// ==++==
+//
+//   Copyright (c) Shadowsoft Corporation.  All rights reserved.
+//
+// ==--==
+
+using Yextly.Scripting.Services.SingleFileApplication;
+
+namespace Yextly.Scripting.Abstractions
+{
+    /// <summary>
+    /// Provides extensions for <see cref="IScriptingExecutionContextBuilder"/>.
+    /// </summary>
+    public static class ScriptingExecutionContextBuilderExtensions
+    {
+        /// <summary>
+        /// Adds the analysis-time types to the provided <see cref="IScriptingExecutionContextBuilder" />.
+        /// </summary>
+        /// <param name="builder">The builder receiving the type references.</param>
+        /// <param name="types">The scripting metadata descriptors.</param>
+        /// <returns></returns>
+        public static IScriptingExecutionContextBuilder AddAnalysisTypes(this IScriptingExecutionContextBuilder builder, ScriptingRuntimeTypes types)
+        {
+            ArgumentNullException.ThrowIfNull(builder);
+            ArgumentNullException.ThrowIfNull(types);
+
+            var source = types.AnalysisTypes;
+
+            AddTypes(builder, source);
+
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds the compile-time types to the provided <see cref="IScriptingExecutionContextBuilder" />.
+        /// </summary>
+        /// <param name="builder">The builder receiving the type references.</param>
+        /// <param name="types">The scripting metadata descriptors.</param>
+        /// <returns></returns>
+        public static IScriptingExecutionContextBuilder AddCompilationTypes(this IScriptingExecutionContextBuilder builder, ScriptingRuntimeTypes types)
+        {
+            ArgumentNullException.ThrowIfNull(builder);
+            ArgumentNullException.ThrowIfNull(types);
+
+            var source = types.CompilationTypes;
+
+            AddTypes(builder, source);
+
+            return builder;
+        }
+
+        private static void AddTypes(IScriptingExecutionContextBuilder builder, ScriptingRuntimeTypeInfo source)
+        {
+            foreach (var item in source.Assemblies)
+            {
+                builder.MetadataReferences.Add(MetadataReferenceCreator.CreateReference(item));
+            }
+
+            foreach (var item in source.Usings)
+            {
+                builder.Usings.Add(item);
+            }
+        }
+    }
+}
---
+++ ScriptingServicesSFA/ScriptingRuntimeTypes.cs
@@ -0,0 +1,24 @@
+﻿// ==++==
+//
+//   Copyright (c) Shadowsoft Corporation.  All rights reserved.
+//
+// ==--==
+
+namespace Yextly.Scripting.Services.SingleFileApplication
+{
+    /// <summary>
+    /// Contains the collection of all the types used both for analysis and compilation by the scripting engine.
+    /// </summary>
+    public sealed record ScriptingRuntimeTypes
+    {
+        /// <summary>
+        /// Contains the analysis-time types.
+        /// </summary>
+        public required ScriptingRuntimeTypeInfo AnalysisTypes { get; init; }
+
+        /// <summary>
+        /// Contains the compilation-time types.
+        /// </summary>
+        public required ScriptingRuntimeTypeInfo CompilationTypes { get; init; }
+    }
+}
